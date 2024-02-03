0e4cdfa46c215aa4c19d61e0468edbb9c5e6ad95
Makoto Nishimura <mac.nishimura@gmail.com>
2024-02-03 20:02:08 +0000

    add 03_02 files

--- MyApi/Program.cs
+++ MyApi/Program.cs
@@ -26,7 +26,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
+app.UseStaticFiles();
 app.MapControllers();
 
 using (var scope = app.Services.CreateScope())
